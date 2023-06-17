b608ad2e2c4bcee227436f6b4f1ccda6b293fda2
Morinke001 <morel1276.mj@gmail.com>
2023-06-17 09:31:31 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+// Write your code here multiply;
+let num1;
+let num2;
+num1=31;
+num2=2;
+let multiply;
+multiply=num1*num2;
+console.log('The value of multiply is:',multiply);
 
-// Write your code here
+let random;
+random=Math.floor((Math.random()*10)+1);
+console.log('The random number is:',random);
+
+let mod;
+let num3;
+let num4;
+num3=10;
+num4=64;
+mod=num4%num3;
+console.log('The mod is:',mod);
+ 
+let max;
+max=Math.max(1,3,4,5,20);
+console.log('The max value is:',max);
